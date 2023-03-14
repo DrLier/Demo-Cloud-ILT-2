4dfcf2031751505b0acd1c3f833cdc1fd599cf33
DrLier <107907898+DrLier@users.noreply.github.com>
2023-03-14 18:48:10 +0800

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,29 @@
+const contacts = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone: "1234567890",
+  },
+  {
+    id: 1,
+    name: 'Bide mama',
+    email: 'bide@example.com',
+    phone: "1111111111",
+  },
+  {
+    id: 1,
+    name: 'Laliu',
+    email: 'Lalius@example.com',
+    phone: "2222222222",
+  },
+  {
+    id: 1,
+    name: 'Bride Jie',
+    email: 'jie@example.com',
+    phone: "3333333333",
+  },
+];
+
+
+module.exports = contacts;
